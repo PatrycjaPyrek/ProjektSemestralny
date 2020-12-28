2120e3895ffb8403192ccaf41c4179eb38297719
Patrycja Pyrek <patipyrek@gmail.com>
2020-12-28 01:22:16 +0100

    Dodano usuwanie klientow na podstawie podanego id

--- ProjektBiblioteka/Management.xaml.cs
+++ ProjektBiblioteka/Management.xaml.cs
@@ -58,12 +58,81 @@ namespace ProjektBiblioteka
         private void ShowButton_Click(object sender, RoutedEventArgs e)
         {
             int index = OptionsComboBox.SelectedIndex;
-            if (index == 0)
+            switch (index)
             {
-              
+                case 0:
+                    break;
+                case 1:
+                    DodawanieUzytkownikowFormularz();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    UsuwanieKlientowFormularz();
+                    break;
+
             }
-            if (index == 1)
+            
+           
+        }
+
+        private void UsuwanieKlientowFormularz()
+        {
+            panel1.Children.Clear();
+            Label nowyKlient = new Label();
+            nowyKlient.Content = "Input id";
+            nowyKlient.FontSize = 15;
+            panel1.Children.Add(nowyKlient);
+            TextBox id = new TextBox();
+            Thickness m = id.Margin;
+            m.Top = -30;
+            m.Left = -70;
+            id.Margin = m;
+            id.Width = 200;
+            id.Height = 25;
+            panel1.Children.Add(id);
+            
+
+            Button submit2 = new Button();
+
+            submit2.Width = 80;
+            submit2.Height = 20;
+            submit2.Content = "Submit";
+            panel1.Children.Add(submit2);
+
+
+
+            submit2.Click += Submit;
+            void Submit(object f, RoutedEventArgs g)
             {
+                int idKlientaDoUsuniecia = Convert.ToInt32(id.Text);
+                var query = context.Klienci.Where(x => x.idKlienta == idKlientaDoUsuniecia).Select(x => x).FirstOrDefault();
+                MessageBoxResult result = MessageBox.Show($"You want to delete client with id = {query.idKlienta}, name ={query.imieKlienta + ' ' + query.nazwiskoKlienta} Please confirm to proceed.", "Confirm deletion", MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        context.Klienci.Remove(query);
+                        context.SaveChanges();
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
+                // string wynik = "";
+                // wynik = klient.idKlienta + klient.imieKlienta + klient.nazwiskoKlienta + klient.NrDowodu;
+                
+
+
+            }
+        }
+
+
+        /// <summary>
+        /// Funkcja dodajaca klientow do bazy
+        /// </summary>
+        private void DodawanieUzytkownikowFormularz()
+        {
+           
                 //id
                 Label nowyKlient = new Label();
                 nowyKlient.Content = "Input id";
@@ -99,18 +168,18 @@ namespace ProjektBiblioteka
                 imie.Height = 25;
                 imie.Margin = m;
                 panel1.Children.Add(imie);
-                
+
                 //nazwisko
                 Label nazwiskoLabel = new Label();
                 nazwiskoLabel.Content = "Input surname";
                 nazwiskoLabel.FontSize = 15;
-                panel1.Children.Add(nazwiskoLabel);                
+                panel1.Children.Add(nazwiskoLabel);
                 TextBox nazwisko = new TextBox();
-                nazwisko.Width =200;
+                nazwisko.Width = 200;
                 nazwisko.Height = 25;
                 nazwisko.Margin = m;
                 panel1.Children.Add(nazwisko);
-               
+
                 //checkbox
                 Label checkboxLabel = new Label();
                 checkboxLabel.Content = "Choose a gender";
@@ -128,14 +197,14 @@ namespace ProjektBiblioteka
                 checkW.Height = 20;
                 checkW.Name = "w";
                 checkW.Content = "Women";
-                
+
                 CheckBox checkM = new CheckBox();
                 checkM.Width = 300;
                 checkM.Margin = n2;
                 checkM.Height = 20;
                 checkM.Name = "w";
                 checkM.Content = "Men";
-               
+
                 panel1.Children.Add(checkW);
                 panel1.Children.Add(checkM);
                 string plec = "K";
@@ -143,7 +212,7 @@ namespace ProjektBiblioteka
                 {
                     plec = "M";
                 }
-                if(checkM.IsChecked==true && checkW.IsChecked == true)
+                if (checkM.IsChecked == true && checkW.IsChecked == true)
                 {
                     throw new Exception("Please choose only one option!");
                 }
@@ -153,7 +222,7 @@ namespace ProjektBiblioteka
                 Thickness t = new Thickness();
                 t.Left = -170;
                 t.Top = -20;
-                DateTime defaultDate = new DateTime(1999,01,01);
+                DateTime defaultDate = new DateTime(1999, 01, 01);
                 Label dataLabel = new Label();
                 dataLabel.Content = "Select birth date";
                 dataLabel.FontSize = 15;
@@ -202,15 +271,15 @@ namespace ProjektBiblioteka
                 panel1.Children.Add(miejscowosc);
                 //submit
                 Button submit2 = new Button();
-              
+
                 submit2.Width = 80;
                 submit2.Height = 20;
                 submit2.Content = "Submit";
                 panel1.Children.Add(submit2);
-             
+
 
                 DateTime data = dataUr.SelectedDate.GetValueOrDefault();
-              
+
                 submit2.Click += Submit;
 
                 // NEW ==============================================
@@ -224,7 +293,7 @@ namespace ProjektBiblioteka
                         imieKlienta = imie.Text,
                         plec = plec,
                         dataUrodzenia = data.Date,
-                        ulica=ulica.Text,
+                        ulica = ulica.Text,
                         kodPocztowy = kod.Text,
                         Miejscowosc = miejscowosc.Text,
                         dataWprowadzenia = DateTime.Now.Date
@@ -235,10 +304,10 @@ namespace ProjektBiblioteka
                     // string wynik = "";
                     // wynik = klient.idKlienta + klient.imieKlienta + klient.nazwiskoKlienta + klient.NrDowodu;
                     MessageBox.Show("Dodano klienta");
-                 
-                    
-                }
 
+
+                }
+            
 
 
 
@@ -252,7 +321,7 @@ namespace ProjektBiblioteka
 
                 //};
                 // MessageBox.Show(id.Text+ imie.Text + nazwisko.Text);
-            }
+            
         }
     }
 }
