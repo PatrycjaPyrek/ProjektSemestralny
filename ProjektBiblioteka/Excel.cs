e049f473117b3c4f50cf477d16de7304ffccd4d8
Patrycja Pyrek <patipyrek@gmail.com>
2021-01-03 13:45:38 +0100

    Dodanie EPPlus do tworzenia arkuszy Excel

--- ProjektBiblioteka/Excel.cs
+++ ProjektBiblioteka/Excel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,28 @@ namespace ProjektBiblioteka
 {
     class Excel
     {
+        public Excel()
+        {
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                excel.Workbook.Worksheets.Add("Month report");
+               
+                var excelWorksheet = excel.Workbook.Worksheets["Worksheet1"];
+                var headerRow = new List<string[]>() {
+                new string[] { "Book id", "Book title", "How many times borrowed", "Data wypożyczenia" } };
+                string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + 1;
+                var worksheet = excel.Workbook.Worksheets["Worksheet1"];
+
+                // Popular header row data
+                worksheet.Cells[headerRange].LoadFromArrays(headerRow);
+
+                FileInfo excelFile = new FileInfo(@"C:\Użytkownicy\test.xlsx");
+                excel.SaveAs(excelFile);
+             
+
+            }
+            
+        }
+        }
     }
-}
+
